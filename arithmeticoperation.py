759eda82378cfd34742d1e641396d060de366e38
Hüseyin Cenik <127469334+huseyincenik@users.noreply.github.com>
2023-03-19 18:33:28 +0300

    Add files via upload

---
+++ arithmeticoperation.py
@@ -0,0 +1,11 @@
+# perform arithmetic operation on two number
+
+a = int(input("Enter one number : "))
+b = int(input("Enter 2nd numbers : "))
+
+print("Addition of two number = ",a + b)
+print("Subtraction of two number = ",a - b)
+print("Multiplication of two number = ",a * b)
+print("Division of two number = ",a / b) #float division
+print("Floor division of two number = ",a // b) #integer division
+print("Mod of two number = ",a % b)
